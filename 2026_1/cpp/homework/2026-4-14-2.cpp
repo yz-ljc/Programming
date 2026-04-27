09af9228142f112def812c66f45d76f05d81ebe8
YZ_Ljc_ <14768819516@163.com>
2026-04-27 13:38:18 +0800

    feat(Web): web

--- 2026_1/cpp/homework/2026-4-14-2.cpp
+++ 2026_1/cpp/homework/2026-4-14-2.cpp
@@ -9,11 +9,11 @@ public:
         this -> id = id;
         this -> balance = balance;
     }
-    void get_account() {
+    void get_account() const {
         cout << "Account ID: " << id << endl;
     }
-    void get_balance() {
-        cout << "Account Balance: " << balance << endl;
+    double get_balance() const {
+        return this->balance;
     }
     void deposit(double amount) {
         this -> balance += amount;
@@ -25,9 +25,34 @@ public:
             return;
         }
         this -> balance -= amount;
-        cout << "Successfully deposited: " << amount << " Now you have " << balance << " in total!" << endl;
+        cout << "Successfully withdrew: " << amount << " Now you have " << balance << " in total!" << endl;
     }
     Account(const Account & oop) : id(oop.id), balance(oop.balance) {}
+};
+
+class Saving {
+private:
+    double min_balance = 0;
+    Account acc;
+public:
+    Saving(int id, double balance, double min_balance) : min_balance(min_balance), acc(id, balance) {}
+    Saving(const Saving & saving_tmp) : min_balance(saving_tmp.min_balance), acc(saving_tmp.acc) {}
+
+    double get_balance() const {
+        return acc.get_balance();
+    }
+
+    double get_min_balance() const {
+        return min_balance;
+    }
+
+    void withdraw(double amount) {
+        acc.taken(amount);
+    }
+
+    void deposit(double amount) {
+        acc.deposit(amount);
+    }
 };
 
 int main() {
@@ -35,16 +60,48 @@ int main() {
     Account acc2(10002, 114514);
     Account acc3(acc2);
 
+    cout << "acc1: ";
     acc1.get_account();
-    acc1.get_balance();
-    acc1.deposit(100);
-    acc1.taken(200);
+    cout << "acc1 balance: " << acc1.get_balance() << endl;
 
+    cout << "acc2: ";
     acc2.get_account();
-    acc2.get_balance();
+    cout << "acc2 balance: " << acc2.get_balance() << endl;
 
+    cout << "acc3 (copied from acc2): ";
     acc3.get_account();
-    acc3.get_balance();
+    cout << "acc3 balance: " << acc3.get_balance() << endl;
+
+    cout << "\n--- 测试存款 ---" << endl;
+    acc1.deposit(100);
+
+    cout << "\n--- 测试取款 ---" << endl;
+    acc1.taken(200);
+    acc1.taken(1000);
+
+    Saving save1(20001, 5000, 1000);
+    Saving save2(20002, 800, 500);
+    Saving save3(save1);
+
+    cout << "save1 - ";
+    cout << "Balance: " << save1.get_balance() << ", Min Balance: " << save1.get_min_balance() << endl;
+
+    cout << "save2 - ";
+    cout << "Balance: " << save2.get_balance() << ", Min Balance: " << save2.get_min_balance() << endl;
+
+    cout << "save3 (copied from save1) - ";
+    cout << "Balance: " << save3.get_balance() << ", Min Balance: " << save3.get_min_balance() << endl;
+
+    cout << "\n--- 测试 Saving 存款 ---" << endl;
+    save1.deposit(2000);
+    cout << "After deposit, save1 balance: " << save1.get_balance() << endl;
+
+    cout << "\n--- 测试 Saving 取款 ---" << endl;
+    save1.withdraw(1000);
+    cout << "After withdraw, save1 balance: " << save1.get_balance() << endl;
+
+    save1.withdraw(10000);
+    cout << "After failed withdraw, save1 balance: " << save1.get_balance() << endl;
 
     return 0;
 }
